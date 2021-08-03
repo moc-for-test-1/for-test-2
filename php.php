661e57c7758b4258e54143e735cf18b47c1776b5
moc-1 <88380684+moc-1@users.noreply.github.com>
2021-08-03 15:12:51 +0300

    Create php.php

---
+++ php.php
@@ -0,0 +1,42 @@
+<?php
+
+/* Предположим, что это страница может быть доступна по http://example.com/operator.php */
+
+class Operator {
+
+    /**
+     * Складываем два операнда
+     * @param interge
+     * @return interge
+     */
+    public function add($a, $b) {
+        return $this->_add($a, $b);
+    }
+
+    /**
+     * Вычитаем
+     */
+    public function sub($a, $b) {
+        return $a - $b;
+    }
+
+    /**
+     * Умножаем
+     */
+    public function mul($a, $b) {
+        return $a * $b;
+    }
+
+    /**
+     * Защищённый метод
+     * @param interge
+     * @return interge
+     */
+    protected function _add($a, $b) {
+        return $a + $b;
+    }
+}
+
+$server = new Yar_Server(new Operator());
+$server->handle();
+?>
